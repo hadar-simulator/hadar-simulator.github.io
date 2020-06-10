cb76fc87d5ca4506ea75f28bb1cc75786adc5171
François Jolain <fjolain@gmail.com>
2020-06-10 15:18:05 +0200

    Improv script python quality

--- exporter/run.sh
+++ exporter/run.sh
@@ -3,17 +3,18 @@
 echo "Download examples from repo"
 wget https://github.com/hadar-simulator/hadar/archive/master.zip
 unzip master.zip
+rm master.zip
 mv hadar-master/examples examples
 rm -r hadar-master
-rm master.zip
 
 echo "Setup python env"
 python3 -m virtualenv venv
 source venv/bin/activate
 pip install -r examples/requirements.txt
+pip install click
 
 echo "Launch exporter"
-python3 exporter.py
+python3 utils.py --src=./examples --export=../assets/notebook
 
 echo "clean"
 rm -r venv
